95a0d0bfa71b28d1b437d75e2fa0d9478392d171
jhfnetboy <jhfnetboy@gmail.com>
2026-06-02 15:57:01 +0700

    fix: clippy — remove redundant & on include_str! in proto/lib.rs

--- kms/proto/src/lib.rs
+++ kms/proto/src/lib.rs
@@ -51,7 +51,7 @@ pub enum Command {
 // If Uuid::parse_str() returns an InvalidLength error, there may be an extra
 // newline in your uuid.txt file. You can remove it by running
 // `truncate -s 36 uuid.txt`.
-pub const UUID: &str = &include_str!("../../uuid.txt");
+pub const UUID: &str = include_str!("../../uuid.txt");
 
 #[cfg(test)]
 mod tests {
